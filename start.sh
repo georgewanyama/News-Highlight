0ab71d847125dfc341042dc0b108fe2283045ce3
georgewanyama <wanyamageorge680@gmail.com>
2019-11-19 11:03:28 +0300

    changed the export class for app

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-export NEWS_API_KEY= '3ab203ae22974681b9c4763691d835f3'
-export SECRET_KEY= 'george124'
+export NEWS_API_KEY="3ab203ae22974681b9c4763691d835f3"
+export SECRET_API_KEY="george124"
+
+python3.6 manage.py server
